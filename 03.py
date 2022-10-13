e2328336c2f57938981eb0f8a4c13d85a06b5368
AlexGoussatchok <Alex.Goussatchok@gmail.com>
2022-10-13 21:17:44 +0300

    Lesson_13

---
+++ 03.py
@@ -0,0 +1,9 @@
+# index    0 1 2 3 4 5 6 7 8 9 10
+# elements 0 1 1 2 3 5 8 13 21 34 55
+
+def fibonacci(index):
+
+    if index < 2:
+        return index
+
+    return fibonacci(index - 1) + fibonacci(index - 2)
